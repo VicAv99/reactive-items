f44b92fae847c342dc61168f759e7179733d3f64
Victor Avila <victoravila2123@gmail.com>
2018-12-15 11:27:47 -0700

    created: main folder layout for components and setup router

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import HomeComponent from './home/home';
+
+const routeComponents = [
+  { key: 1, path: '/home/', component: HomeComponent }
+];
+
+const routes = routeComponents.map(route => (
+  <Route key={route.key} path={route.path} component={route.component}/>
+));
+
+const AppRouter = () => (
+  <Router>
+    <div>
+
+      {routes}
+    </div>
+  </Router>
+);
+
+export default Router;
