84a79f742550aeb81040691e37308ae4f0663abf
Victor Avila <victoravila2123@gmail.com>
2018-12-17 05:24:44 -0700

    added: display widgets

--- src/widgets/widgets-list/widgets-list.js
+++ src/widgets/widgets-list/widgets-list.js
@@ -5,20 +5,32 @@ import { connect } from 'react-redux';
 
 export class WidgetsList extends Component {
   static propTypes = {
-
+    widgets: PropTypes.array.isRequired
   }
 
   render() {
+    const { widgets } = this.props;
+    const widgetsList = widgets.map(widget =>
+      <div key={widget.id} className="list-group">
+        <div className="list-group-item list-group-item-action">
+          {widget.name} <br/>
+          {widget.description}
+          <span className="badge badge-danger badge-pill float-right">
+            X
+          </span>
+        </div>
+      </div>
+    )
     return (
       <div>
-        list
+        {widgetsList}
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-
+  widgets: state.widgets.widgets
 })
 
 const mapDispatchToProps = {
--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -3,34 +3,41 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './widgets.css';
 import WidgetsList from './widgets-list/widgets-list';
 import WidgetsDetails from './widgets-details/widgets-details';
+import { fetchWidgets } from '../state/actions/widgetActions';
 
 export class WidgetsComponent extends Component {
-  static propTypes = {
-
+  componentWillMount() {
+    this.props.fetchWidgets();
   }
 
+  static propTypes = {
+    widgets: PropTypes.array.isRequired
+  };
+
   render() {
+    const { widgets } = this.props;
     return (
       <div className="wrapper">
-        <div>
-          <WidgetsList />
+        <div className="list-grid">
+          <WidgetsList widgets={widgets} />
         </div>
-        <div>
+        <div className="details-grid">
           <WidgetsDetails />
         </div>
       </div>
     )
-  }
+  };
 }
 
 const mapStateToProps = (state) => ({
-
-})
+  widgets: state.widgets.widgets
+});
 
 const mapDispatchToProps = {
+  fetchWidgets
+};
 
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(WidgetsComponent)
+export default connect(mapStateToProps, mapDispatchToProps)(WidgetsComponent);
